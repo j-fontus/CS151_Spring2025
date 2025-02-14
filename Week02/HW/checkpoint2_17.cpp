e0f69a94b41a2e827af6eec997a6df621f5e0fee
j-fontus <josue.fontus@mec.science>
2025-02-14 18:54:46 -0500

    Update checkpoint2_17.cpp

--- Week02/HW/checkpoint2_17.cpp
+++ Week02/HW/checkpoint2_17.cpp
@@ -2,3 +2,19 @@
 Write a program that stores your name, address, and phone number in three
 separate string objects. Display the contents of the string objects on the screen.
 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name = "Joe Fon";
+    string address = "226 Linden Blvd, Brooklyn, NY";
+    string phoneNumber = "347-820-2924";
+
+    cout << "Name: " << name << endl;
+    cout << "Address: " << address << endl;
+    cout << "Phone Number: " << phoneNumber << endl;
+
+    return 0;
+}
