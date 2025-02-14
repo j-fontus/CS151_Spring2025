0e896f49000887ddb59edf0c6f99c5a2787f6d98
j-fontus <josue.fontus@mec.science>
2025-02-14 20:05:44 +0000

    Repository Update - 2025-02-14 20:05:44

---
+++ Week03/week03.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+<<<<<<< HEAD
+=======
+#include <string>
+>>>>>>> upstream/main
+
+using namespace std;
+
+int main()
+<<<<<<< HEAD
+
+{
+    cout<< "Hello, World";
+=======
+{
+    /*
+    string hello = "Hello, World!";
+    char letter = 'A';
+    cout << sizeof(letter) << endl;
+    cout << sizeof("A") << endl;
+    string letter_a = "A";
+    cout << sizeof(letter_a) << endl;
+    */
+
+    // cout << hello << endl;
+
+    bool check = 67;
+    cout << boolalpha << check << endl;
+
+    auto word = "test";
+    cout << word << endl;
+
+    const double PI = 3.1415; // This is pi
+    // PI = 3.14159;
+    cout << PI << endl;
+
+
+>>>>>>> upstream/main
+}
