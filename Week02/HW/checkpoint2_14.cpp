ec88b1dc29fbad68ced6362952bc4ff32df0b34b
j-fontus <josue.fontus@mec.science>
2025-02-14 18:36:38 -0500

    Update checkpoint2_14.cpp

--- Week02/HW/checkpoint2_14.cpp
+++ Week02/HW/checkpoint2_14.cpp
@@ -2,3 +2,17 @@
 Write a program that has the following character variables: first, middle, and
 last. Store your initials in these variables then display them on the screen.
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char first = 'J'; // Replace 'J' with your first initial
+    char middle = 'M'; // Replace 'M' with your middle initial
+    char last = 'F'; // Replace 'F' with your last initial
+
+    cout << "My initials are: " << first << middle << last << endl;
+
+    return 0;
+}
