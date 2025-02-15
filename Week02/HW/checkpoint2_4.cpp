35bd5f2936b37e4ec20d4065487c5be96ecc4486
j-fontus <josue.fontus@mec.science>
2025-02-14 19:58:23 -0500

    Update checkpoint2_4.cpp

--- Week02/HW/checkpoint2_4.cpp
+++ Week02/HW/checkpoint2_4.cpp
@@ -4,3 +4,13 @@ on the second line, your city, state, and ZIP code on the third line, and your
 telephone number on the fourth line. Place a comment with today’s date at the top
 of the program. Test your program by compiling and running it.
 */
+
+#include <isostream>
+using namespace std;
+
+int main()
+{
+  cout << "Joe Fon " << endl;
+  cout << "226 Linden Blvd"<< endl;
+  cout << "347-820-2924"<< endl;
+return 0;
