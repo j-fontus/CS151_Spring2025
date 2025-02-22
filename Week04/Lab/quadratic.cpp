6a342f817257f170b88eda41902c736b2b9216ab
j-fontus <josue.fontus@mec.science>
2025-02-22 19:54:49 +0000

    Repository Update - 2025-02-22 19:54:49

--- Week04/Lab/quadratic.cpp
+++ Week04/Lab/quadratic.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
-#include <string>
 #include <cmath>
 
 using namespace std;
 
 int main()
 {
-    int a;
-    int b;
-    int c;
+    double a, b, c;
+    double discriminant, x1, x2;
 
-    cin >> 
+    cin >> a;
+    cin >> b;
+    cin >> c;
 
+    discriminant = pow(b, 2) - (4 * a * c);
 
+    x1 = (-b + sqrt(discriminant))/ (2 * a);
 
+    x2 = (-b+ sqrt(discriminant))/ (2 * a);
 
+    cout << x1 << endl;
+    cout << x2 << endl;
 
-
+    return 0;
 
 }
