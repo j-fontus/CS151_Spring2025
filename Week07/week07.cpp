b1549c6d2487fd9b96d86ab1a51637faf6490b31
j-fontus <josue.fontus@mec.science>
2025-03-14 18:55:34 +0000

    Repository Update - 2025-03-14 18:55:33

--- Week07/week07.cpp
+++ Week07/week07.cpp
@@ -8,5 +8,27 @@
 using namespace std;
 
 int main() {
+    int choice
+    cout << "Please enter the quiz number (1-6) for the extra";
+    cout << "credit you would like to see:";
+    cin >> choice;
+
+    switch (choice)
+    {
+        case 1: cout << "choice 1\n";
+                break;
+        case 2: cout << "choice 2\n";
+                break;
+        case 3: cout << "choice 3\n";
+                break;
+        case 4: cout << "choice 4\n";
+                break;
+        case 5: cout << "choice 5\n";
+                break;
+        case 6 cout << "choice 6\n";
+                break;
+        default: cout << "invaled choice\n";
+    }        
+    }       
     
-}
+    
