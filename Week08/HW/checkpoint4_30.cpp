cdf331151bbc6a232b168e4eae0a83f1a737504a
j-fontus <josue.fontus@mec.science>
2025-03-28 00:37:32 +0000

    Repository Update - 2025-03-28 00:37:32

--- Week08/HW/checkpoint4_30.cpp
+++ Week08/HW/checkpoint4_30.cpp
@@ -7,13 +7,27 @@
 
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int userNum;
     cout << "Enter one of the numbers 1, 2, or 3: ";
     cin >> userNum;
-    //
-    // Write the switch statement here.
-    //
+
+    switch (userNum)
+    {
+        case 1:
+            cout << "one" << endl;
+            break;
+        case 2:
+            cout << "two" << endl;
+            break;
+        case 3:
+            cout << "three" << endl;
+            break;
+        default:
+            cout << "Error: Invalid number entered." << endl;
+    }
+
     return 0;
 }
--- Week08/HW/checkpoint4_31.cpp
+++ Week08/HW/checkpoint4_31.cpp
@@ -11,15 +11,24 @@ int main()
     cout << "3. Area of a cylinder\n";
     cout << "4. None of them!\n";
     cin >> selection;
-    if (selection == 1)
-        cout << "Pi times radius squared\n";
-    else if (selection == 2)
-        cout << "Length times width\n";
-    else if (selection == 3)
-        cout << "Pi times radius squared times height\n";
-    else if (selection == 4)
-        cout << "Well okay then, good bye!\n";
-    else
-        cout << "Not good with numbers, eh?\n";
+
+    switch (selection)
+    {
+        case 1:
+            cout << "Pi times radius squared\n";
+            break;
+        case 2:
+            cout << "Length times width\n";
+            break;
+        case 3:
+            cout << "Pi times radius squared times height\n";
+            break;
+        case 4:
+            cout << "Well okay then, good bye!\n";
+            break;
+        default:
+            cout << "Not good with numbers, eh?\n";
+    }
+
     return 0;
 }
