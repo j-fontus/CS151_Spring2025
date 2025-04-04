b14aa2f6535ddff1699611bd312ee382e5f1149c
j-fontus <josue.fontus@mec.science>
2025-04-04 19:49:57 +0000

    Repository Update - 2025-04-04 19:49:57

---
+++ Week10/Lab/fibonacci.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int jo; 
+    int fi; 
+    int bo;
+    int cc;
+
+    fi = 0;
+    bo = 1;
+
+    for (int i = 2; i <= jo; i++)
+    {
+        cout << "enter a non negative integer" << endl;
+        cin >> jo
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+
+}
