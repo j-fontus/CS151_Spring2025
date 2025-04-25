2235d37476ef5b8f76f2d18fa6b917cb94bfcb03
j-fontus <josue.fontus@mec.science>
2025-04-25 20:56:09 +0000

    Repository Update - 2025-04-25 20:56:09

--- Week11/Lab/helper.cpp
+++ Week11/Lab/helper.cpp
@@ -26,7 +26,7 @@ string allLower(string str) {
     for (int i = 0; i < str.length(); i++) {
         str[i] = tolower(str[i]);
     }
-    return str;
+    return str; 
 }
 
 
--- Week13/Lab/func.cpp
+++ Week13/Lab/func.cpp
@@ -5,3 +5,63 @@
 
 using namespace std;
 
+void sayHello(string = "World");
+void allUpper(string &name);
+void alllower(string &name);
+int getRandom(int max, int min = 0);
+
+int main()
+{
+ sayHello();
+ sayHello("Eve");
+ string A = "Heyjoe";
+ allUpper(A);
+ cout << A << endl;
+ alllower(A);
+ cout << A << endl;
+ cout << getRandom(20) << endl;
+
+    return 0;
+}
+void sayHello(string name)
+{
+cout << "Hello, " << name << "!" << endl;
+}
+
+void allUpper(string &name)
+{
+    for (int i = 0; i < name.length(); i++)
+    {
+        name[i] = toupper(name[i]);
+    }
+    
+}
+void alllower(string &name)
+{
+    for (int i = 0; i < name.length(); i++)
+    {
+        name[i] = tolower(name[i]);
+    }
+     
+}
+int getRandom(int max, int min)
+{
+    static bool seeded = false;
+    if (seeded == false)
+    {
+    srand(time(0)); 
+    seeded = true;
+    }
+    if (max < min)
+    {
+ int temp = min; 
+ min = max;
+ max = temp;
+    }
+ return rand()%(max + 1 - min)+ min;
+
+ 
+}
+
+ 
+    
